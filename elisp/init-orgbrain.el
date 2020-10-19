6ff0e565c14f86396b9b332639b2d4e5ff37a8c8
zorowk <zoro@zoro-mi.localdomain>
2020-10-20 00:52:10 +0800

    add org brain model
    添加org－brain的配置
    
    Signed-off-by: zorowk <zoro@zoro-mi.localdomain>

--- elisp/init-orgbrain.el
+++ elisp/init-orgbrain.el
@@ -56,9 +56,29 @@
        (`weekly "#+TITLE: Weekly Journal\n#+STARTUP: folded \n\n")
        (`monthly "#+TITLE: Monthly Journal\n#+STARTUP: folded \n\n")
        (`yearly "#+TITLE: Yearly Journal\n#+STARTUP: folded \n\n"))))
-  (setq org-journal-file-header 'org-journal-file-header-func)
-)
+  (setq org-journal-file-header 'org-journal-file-header-func))
 ;; -org journal
+
+;; org brain
+(use-package org-brain :ensure t
+  :init
+  (setq org-brain-path "~/Dropbox/brain/")
+  ;; For Evil users
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  :config
+  (bind-key "C-c b" 'org-brain-prefix-map org-mode-map)
+  (setq org-id-track-globally t)
+  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  (add-hook 'before-save-hook #'org-brain-ensure-ids-in-buffer)
+  (push '("b" "Brain" plain (function org-brain-goto-end)
+          "* %i%?" :empty-lines 1)
+        org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12)
+  (setq org-brain-include-file-entries nil
+        org-brain-file-entries-use-title nil))
+;; -org brain
 
 (provide 'init-orgbrain)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
