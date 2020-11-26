932d62d762aeeeadb613b7f60188a478846c38ea
zorowk <near.kingzero@gmail.com>
2020-11-27 00:20:45 +0800

    remove org-journal replace calendar
    Signed-off-by: zorowk <near.kingzero@gmail.com>

--- elisp/init-orgmisc.el
+++ elisp/init-orgmisc.el
@@ -35,30 +35,6 @@
 ;;
 ;;; Code:
 
-;; org journal
-(use-package org-journal
-  :ensure t
-  :after org
-  :init
-  ;; Change default prefix key; needs to be set before loading org-journal
-  (setq org-journal-prefix-key "C-c j ")
-  :config
-  (setq org-journal-dir "~/Dropbox/journal/"
-        org-journal-date-format "%A, %d %B %Y"
-        org-journal-file-format "%Y-%m-%d.org"
-        org-journal-file-type 'weekly)
-
-  (defun org-journal-file-header-func (time)
-    "Custom function to create journal header."
-    (concat
-     (pcase org-journal-file-type
-       (`daily "#+TITLE: Daily Journal\n#+STARTUP: showeverything \n\n")
-       (`weekly "#+TITLE: Weekly Journal\n#+STARTUP: folded \n\n")
-       (`monthly "#+TITLE: Monthly Journal\n#+STARTUP: folded \n\n")
-       (`yearly "#+TITLE: Yearly Journal\n#+STARTUP: folded \n\n"))))
-  (setq org-journal-file-header 'org-journal-file-header-func))
-;; -org journal
-
 ;; org-ref
 (use-package org-ref
   :after org
