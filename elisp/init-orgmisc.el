01753eaa493dbf6b543fdc0064742c6473527205
zorowk <near.kingzero@gmail.com>
2021-07-21 14:25:02 +0800

    fix: modify org-roam usage
    Signed-off-by: zorowk <near.kingzero@gmail.com>

--- elisp/init-orgmisc.el
+++ elisp/init-orgmisc.el
@@ -73,18 +73,17 @@
 ;; org roam
 (use-package org-roam
       :ensure t
-      :hook
-      (after-init . org-roam-mode)
       :custom
       (org-roam-directory "~/Dropbox/notes/")
-      :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-graph))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))
-              (("C-c n I" . org-roam-insert-immediate)))
+      :bind (("C-c n l" . org-roam-buffer-toggle)
+             ("C-c n f" . org-roam-node-find)
+             ("C-c n g" . org-roam-graph)
+             ("C-c n i" . org-roam-node-insert)
+             ("C-c n c" . org-roam-capture)
+             ;; Dailies
+             ("C-c n j" . org-roam-dailies-capture-today))
       :config
+      (org-roam-setup)
       (require 'org-roam-protocol)
       (setq org-roam-dailies-capture-templates
             '(("d" "default" entry
@@ -93,24 +92,10 @@
                :file-name "daily/%<%Y-%m-%d>"
                :head "#+title: %<%Y-%m-%d>\n\n"))))
 
-(use-package org-roam-server
-  :ensure t
-  :config
-  (setq org-roam-server-host "127.0.0.1"
-        org-roam-server-port 8080
-        org-roam-server-authenticate nil
-        org-roam-server-export-inline-images t
-        org-roam-server-serve-files nil
-        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-        org-roam-server-network-poll t
-        org-roam-server-network-arrows nil
-        org-roam-server-network-label-truncate t
-        org-roam-server-network-label-truncate-length 60
-        org-roam-server-network-label-wrap-length 20))
-
 (use-package org-roam-bibtex
   :after org-roam
-  :hook (org-roam-mode . org-roam-bibtex-mode))
+  :config
+  (require 'org-ref))
 ;; -org roam
 
 ;; org bullets
