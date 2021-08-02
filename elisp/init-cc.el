ee8245619f190f1c060c1073f4f76688ed0d4827
Mingde (Matthew) Zeng <matthewzmd@posteo.net>
2021-08-02 14:52:04 -0400

    Add rust, vue, update with EAF changes
    Signed-off-by: Mingde (Matthew) Zeng <matthewzmd@posteo.net>

--- elisp/init-cc.el
+++ elisp/init-cc.el
@@ -80,6 +80,21 @@
   :ensure t
   :mode (("CMakeLists\\.txt\\'" . cmake-mode) ("\\.cmake\\'" . cmake-mode)))
 ;; -cmake
+
+;; RustPac
+(use-package rust-mode
+  :mode "\\.rs\\'"
+  :custom
+  (rust-format-on-save t)
+  :bind (:map rust-mode-map ("C-c C-c" . rust-run))
+  :config
+  (use-package flycheck-rust
+    :after flycheck
+    :config
+    (with-eval-after-load 'rust-mode
+      (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))))
+;; -RustPac
+
 (provide 'init-cc)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init-cc.el ends here
