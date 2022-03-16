ca161b1c9fad9e8fcb94cf13bc54b8c79dd8d77d
pengwenhao <pengwenhao@uniontech.com>
2022-03-17 00:32:36 +0800

    update lsp threshold
    Signed-off-by: pengwenhao <pengwenhao@uniontech.com>

--- elisp/init-lsp.el
+++ elisp/init-lsp.el
@@ -45,6 +45,8 @@
   (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
   (lsp-enable-file-watchers nil)
   (lsp-enable-folding nil)
+  (lsp-lens-enable nil)
+  (gc-cons-threshold (* 1024 1024 1024))
   (read-process-output-max (* 1024 1024))
   (lsp-keep-workspace-alive nil)
   (lsp-eldoc-hook nil)
