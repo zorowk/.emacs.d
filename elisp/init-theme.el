550428c83efcc42e360532c7ce05184cd2203ac2
pengwenhao <pengwenhao@uniontech.com>
2024-09-09 10:28:45 +0800

    fix: org head line
    Signed-off-by: pengwenhao <pengwenhao@uniontech.com>

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -45,25 +45,13 @@
   :demand t
   :config
   (setq ef-themes-variable-pitch-ui t
-        ef-themes-mixed-fonts t
-        ef-themes-headings ; read the manual's entry of the doc string
-        '((0 . (variable-pitch light 1.9))
-          (1 . (variable-pitch light 1.8))
-          (2 . (variable-pitch regular 1.7))
-          (3 . (variable-pitch regular 1.6))
-          (4 . (variable-pitch regular 1.5))
-          (5 . (variable-pitch 1.4)) ; absence of weight means `bold'
-          (6 . (variable-pitch 1.3))
-          (7 . (variable-pitch 1.2))
-          (agenda-date . (semilight 1.5))
-          (agenda-structure . (variable-pitch light 1.9))
-          (t . (variable-pitch 1.1))))
+        ef-themes-mixed-fonts t)
 
   ;; Disable all other themes to avoid awkward blending:
   (mapc #'disable-theme custom-enabled-themes)
 
   ;; Load the theme of choice:
-  (load-theme 'ef-spring :no-confirm))
+  (load-theme 'ef-light :no-confirm))
 ;; -EfThemes
 
 ;; DoomModeline
