c3a909c531639595f104045e6500a15b8cb84c21
zorowk <pengwenhao@uniontech.com>
2025-10-06 00:54:19 +0800

    add dumb jump

--- init.el
+++ init.el
@@ -148,6 +148,8 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
 
 (require 'init-yasnippet)
 
+(require 'init-dumb-jump)
+
 (require 'init-treesit)
 
 (require 'init-indent)
