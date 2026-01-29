c4490b2a20b2a2b89721f76111f12f8512e4797e
zoro wk <pengwenhao@uniontech.com>
2026-01-29 15:20:01 +0800

    add vundo

--- elisp/init-undo-tree.el
+++ elisp/init-undo-tree.el
@@ -36,14 +36,8 @@
 ;;; Code:
 
 ;; UndoTreePac
-(use-package undo-tree
-  :defer t
-  :diminish undo-tree-mode
-  :init (global-undo-tree-mode)
-  :custom
-  (undo-tree-visualizer-diff t)
-  (undo-tree-history-directory-alist `(("." . ,(expand-file-name ".backup" user-emacs-directory))))
-  (undo-tree-visualizer-timestamps t))
+(use-package vundo
+  :defer t)
 ;; -UndoTreePac
 
 (provide 'init-undo-tree)
