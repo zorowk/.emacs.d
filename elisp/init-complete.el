a90a3924fbd6190a1fc3a2c232942a2a0d6cfa34
zoro wk <pengwenhao@uniontech.com>
2026-01-30 10:13:40 +0800

    update corfu configure

--- elisp/init-complete.el
+++ elisp/init-complete.el
@@ -81,29 +81,59 @@
   (:map eglot-mode-map
 	("C-c f" . consult-eglot-symbols)))
 
-;; Enable Corfu completion UI
-;; See the Corfu README for more configuration tips.
+;; Corfu
 (use-package corfu
   :straight t
   :custom
-  (corfu-auto t)          ;; 启用自动补全
-  (corfu-auto-prefix 3)   ;; 触发补全的最小字符数
-  (corfu-auto-delay 0.1)  ;; 补全延迟
-  (corfu-popupinfo-delay '(0.5 . 0.2)) ;; 补全信息延迟
+  (corfu-auto t)
+  (corfu-auto-prefix 3)
+  (corfu-auto-delay 0.1)
+  (corfu-popupinfo-delay '(0.5 . 0.2))
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+
   :init
+  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
+  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
+  ;; variable `global-corfu-modes' to exclude certain modes.
   (global-corfu-mode)
-  (corfu-popupinfo-mode 1) ;; 显示补全详细信息
-  :bind (:map corfu-map
-              ("TAB" . corfu-next)
-              ([tab] . corfu-next)
-              ("S-TAB" . corfu-previous)
-              ([backtab] . corfu-previous)))
+
+  ;; Enable optional extension modes:
+  (corfu-history-mode)
+  (corfu-popupinfo-mode)
+
+  ;; Use TAB for cycling, default is `corfu-complete'.
+  :bind
+  (:map corfu-map
+        ("TAB" . corfu-next)
+        ([tab] . corfu-next)
+        ("S-TAB" . corfu-previous)
+        ([backtab] . corfu-previous)))
+
+;; A few more useful configurations...
+(use-package emacs
+  :custom
+  ;; TAB cycle if there are only few candidates
+  ;; (completion-cycle-threshold 3)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (tab-always-indent 'complete)
+
+  ;; Emacs 30 and newer: Disable Ispell completion function.
+  ;; Try `cape-dict' as an alternative.
+  (text-mode-ispell-word-completion nil)
+
+  ;; Hide commands in M-x which do not apply to the current mode.  Corfu
+  ;; commands are hidden, since they are not used via M-x. This setting is
+  ;; useful beyond Corfu.
+  (read-extended-command-predicate #'command-completion-default-include-p))
 
 (use-package corfu-terminal
   :straight (corfu-terminal :type git :repo "https://codeberg.org/akib/emacs-corfu-terminal.git")
   :init
   (unless (display-graphic-p)
-  (corfu-terminal-mode +1)))
+    (corfu-terminal-mode +1)))
+;; -CorFu
 
 ;; Add extensions
 (use-package cape
@@ -123,8 +153,11 @@
   (add-hook 'completion-at-point-functions #'cape-dabbrev)
   (add-hook 'completion-at-point-functions #'cape-file)
   (add-hook 'completion-at-point-functions #'cape-elisp-block)
-  (add-hook 'completion-at-point-functions #'cape-keyword) ;; 关键字补全
-  )
+  (add-hook 'completion-at-point-functions #'cape-keyword)
+
+  :config
+  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster)
+  (advice-add 'eglot-completion-at-point :around #'cape-wrap-nonexclusive))
 
 ;; Enable rich annotations using the Marginalia package
 (use-package marginalia
