efd2437b5586edc1c87b7819e27bf3793e63dc24
zoro wk <pengwenhao@uniontech.com>
2026-01-31 15:08:10 +0800

    remove tramp configure

--- elisp/init-erc.el
+++ elisp/init-erc.el
@@ -44,12 +44,9 @@
   :straight (:type built-in)
   :defer t
   :custom
-  ;; 基本身份信息（请手动设置你的昵称）
-  (erc-nick "your-nick-here")           ; ← 修改成你的昵称
+  (erc-nick "zorowk")
   (erc-user-full-name user-full-name)
   (erc-autojoin-channels-alist '(("irc.libera.chat" "#emacs")))
-
-  ;; 显示与日志
   (erc-interpret-mirc-color t)
   (erc-fill-function 'erc-fill-static)
   (erc-fill-static-center 15)
@@ -57,53 +54,23 @@
   (erc-server-coding-system '(utf-8 . utf-8))
   (erc-log-channels-directory (expand-file-name ".erc-logs" user-emacs-directory))
   (erc-save-buffer-on-part t)
-
-  ;; 行为优化
   (erc-track-exclude-types '("NICK" "PART" "MODE" "324" "329" "332" "333" "353" "477"))
   (erc-kill-buffer-on-part t)
   (erc-kill-queries-on-quit t)
   (erc-kill-server-buffer-on-quit t)
-  (erc-lurker-threshold-time 43200)     ; 12 小时视为 lurker
+  (erc-lurker-threshold-time 43200)
   (erc-server-reconnect-attempts 5)
   (erc-server-reconnect-timeout 3)
   (erc-nick-uniquifier "_")
-
-  ;; 通知与高亮
-  (erc-modules '(notifications hl-nicks image services track))  ; 内置 hl-nicks 已可用
-
-  ;; 外观
+  (erc-modules '(notifications hl-nicks image services track))
   :custom-face
   (erc-notice-face ((t (:foreground "#ababab"))))
-
-  :bind
-  (("C-c i"   . erc-start-or-switch)     ; 启动或切换（推荐一个快捷键）
-   ("C-c C-b" . erc-switch-to-buffer)
-   (:map erc-mode-map
-         ("M-RET" . newline)))
-
   :hook
   (ercn-notify . erc-notify)
-
   :config
-  ;; 确保日志目录存在
   (make-directory erc-log-channels-directory t)
-
-  ;; 启用内置模块
   (erc-track-mode 1)
   (erc-services-mode 1)
-
-  ;; 启动或切换函数（简化版）
-  (defun erc-start-or-switch ()
-    "Start ERC on Libera.Chat or switch to existing buffer."
-    (interactive)
-    (if (get-buffer "irc.libera.chat:6697")
-        (erc-track-switch-buffer 1)
-      (erc-tls :server "irc.libera.chat"
-               :port 6697
-               :nick erc-nick
-               :full-name erc-user-full-name)))
-
-  ;; 通知函数（使用 alert 包或内置 notify）
   (defun erc-notify (nickname message)
     "Show notification for ERC message."
     (let* ((channel (buffer-name))
