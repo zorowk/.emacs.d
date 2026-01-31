04f52997370e4da6e694d49aff5c15409c5663db
zoro wk <pengwenhao@uniontech.com>
2026-01-31 14:13:41 +0800

    modify erc configure

--- elisp/init-erc.el
+++ elisp/init-erc.el
@@ -42,66 +42,77 @@
 ;; ERCPac
 (use-package erc
   :straight (:type built-in)
-  :init
-  ;; Prerequisite: Configure this to your IRC nickname
-  (defcustom my-irc-nick ""
-    "The nickname used to login into ERC"
-    :type 'string)
-  (use-package erc-hl-nicks :defer t)
-  (use-package erc-image :defer t)
-  :custom-face
-  (erc-notice-face ((t (:foreground "#ababab"))))
+  :defer t
   :custom
-  (erc-autojoin-channels-alist '(("irc.libera.chat" "#emacs")))
+  ;; 基本身份信息（请手动设置你的昵称）
+  (erc-nick "your-nick-here")           ; ← 修改成你的昵称
   (erc-user-full-name user-full-name)
-  (erc-track-exclude-types '("NICK" "PART" "MODE" "324" "329" "332" "333" "353" "477"))
-  (erc-server-coding-system '(utf-8 . utf-8))
+  (erc-autojoin-channels-alist '(("irc.libera.chat" "#emacs")))
+
+  ;; 显示与日志
   (erc-interpret-mirc-color t)
+  (erc-fill-function 'erc-fill-static)
+  (erc-fill-static-center 15)
+  (erc-fill-column 100)
+  (erc-server-coding-system '(utf-8 . utf-8))
+  (erc-log-channels-directory (expand-file-name ".erc-logs" user-emacs-directory))
+  (erc-save-buffer-on-part t)
+
+  ;; 行为优化
+  (erc-track-exclude-types '("NICK" "PART" "MODE" "324" "329" "332" "333" "353" "477"))
   (erc-kill-buffer-on-part t)
   (erc-kill-queries-on-quit t)
   (erc-kill-server-buffer-on-quit t)
-  (erc-autojoin-timing 'ident)
-  (erc-fill-function 'erc-fill-static)
-  (erc-fill-static-center 15)
-  (erc-lurker-threshold-time 43200)
+  (erc-lurker-threshold-time 43200)     ; 12 小时视为 lurker
   (erc-server-reconnect-attempts 5)
   (erc-server-reconnect-timeout 3)
-  (erc-prompt-for-password nil)
-  (erc-prompt-for-nickserv-password nil)
-  (erc-fill-column 100)
-  (erc-save-buffer-on-part t)
   (erc-nick-uniquifier "_")
-  (erc-log-channels-directory (expand-file-name ".erc-logs" user-emacs-directory))
+
+  ;; 通知与高亮
+  (erc-modules '(notifications hl-nicks image services track))  ; 内置 hl-nicks 已可用
+
+  ;; 外观
+  :custom-face
+  (erc-notice-face ((t (:foreground "#ababab"))))
+
   :bind
-  (("M-z i" . erc-start-or-switch)
-   ("M-m i" . erc-start-or-switch)
+  (("C-c i"   . erc-start-or-switch)     ; 启动或切换（推荐一个快捷键）
    ("C-c C-b" . erc-switch-to-buffer)
    (:map erc-mode-map
          ("M-RET" . newline)))
+
   :hook
   (ercn-notify . erc-notify)
+
   :config
-  (make-directory (expand-file-name ".erc-logs" user-emacs-directory) t)
-  (add-to-list 'erc-modules 'notifications)
-  (erc-track-mode t)
+  ;; 确保日志目录存在
+  (make-directory erc-log-channels-directory t)
+
+  ;; 启用内置模块
+  (erc-track-mode 1)
   (erc-services-mode 1)
+
+  ;; 启动或切换函数（简化版）
   (defun erc-start-or-switch ()
-    "Start ERC or switch to ERC buffer if it has started already."
+    "Start ERC on Libera.Chat or switch to existing buffer."
     (interactive)
     (if (get-buffer "irc.libera.chat:6697")
         (erc-track-switch-buffer 1)
-      (erc-tls :server "irc.libera.chat" :port 6697 :nick my-irc-nick :full-name user-full-name)))
+      (erc-tls :server "irc.libera.chat"
+               :port 6697
+               :nick erc-nick
+               :full-name erc-user-full-name)))
 
+  ;; 通知函数（使用 alert 包或内置 notify）
   (defun erc-notify (nickname message)
-    "Displays a notification message for ERC."
+    "Show notification for ERC message."
     (let* ((channel (buffer-name))
-           (nick (erc-hl-nicks-trim-irc-nick nickname))
+           (nick (or (erc-hl-nicks-trim-irc-nick nickname) nickname))
            (title (if (string-match-p (concat "^" nickname) channel)
                       nick
-                    (concat nick " (" channel ")")))
-           (msg (s-trim (s-collapse-whitespace message))))
+                    (format "%s (%s)" nick channel)))
+           (msg (string-trim (replace-regexp-in-string "[ \t\n]+" " " message))))
       (alert (concat nick ": " msg) :title title))))
-;; -ERCPac
 
 (provide 'init-erc)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
