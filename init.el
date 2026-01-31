64d367301bcd0fc532e7022bb4e00a227a5ff16c
zoro wk <pengwenhao@uniontech.com>
2026-01-31 11:41:16 +0800

    remove crux

--- init.el
+++ init.el
@@ -116,8 +116,6 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
 
 (require 'init-search)
 
-(require 'init-crux)
-
 (require 'init-avy)
 
 (require 'init-which-key)
