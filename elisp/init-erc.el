256f3aa4223075b0e80dcf864f24dbb08247f0ba
zoro wk <pengwenhao@uniontech.com>
2026-02-26 15:04:01 +0800

    update erc

--- elisp/init-erc.el
+++ elisp/init-erc.el
@@ -42,44 +42,66 @@
 ;; ERCPac
 (use-package erc
   :straight (:type built-in)
-  :defer t
+  :init
+  ;; Prerequisite: Configure this to your IRC nickname
+  (defcustom my-irc-nick ""
+    "The nickname used to login into ERC"
+    :type 'string)
+  (use-package erc-hl-nicks :defer t)
+  (use-package erc-image :defer t)
+  :custom-face
+  (erc-notice-face ((t (:foreground "#ababab"))))
   :custom
-  (erc-nick "zorowk")
-  (erc-user-full-name user-full-name)
   (erc-autojoin-channels-alist '(("irc.libera.chat" "#emacs")))
-  (erc-interpret-mirc-color t)
-  (erc-fill-function 'erc-fill-static)
-  (erc-fill-static-center 15)
-  (erc-fill-column 100)
-  (erc-server-coding-system '(utf-8 . utf-8))
-  (erc-log-channels-directory (expand-file-name ".erc-logs" user-emacs-directory))
-  (erc-save-buffer-on-part t)
+  (erc-user-full-name user-full-name)
   (erc-track-exclude-types '("NICK" "PART" "MODE" "324" "329" "332" "333" "353" "477"))
+  (erc-server-coding-system '(utf-8 . utf-8))
+  (erc-interpret-mirc-color t)
   (erc-kill-buffer-on-part t)
   (erc-kill-queries-on-quit t)
   (erc-kill-server-buffer-on-quit t)
+  (erc-autojoin-timing 'ident)
+  (erc-fill-function 'erc-fill-static)
+  (erc-fill-static-center 15)
   (erc-lurker-threshold-time 43200)
   (erc-server-reconnect-attempts 5)
   (erc-server-reconnect-timeout 3)
+  (erc-prompt-for-password nil)
+  (erc-prompt-for-nickserv-password nil)
+  (erc-fill-column 100)
+  (erc-save-buffer-on-part t)
   (erc-nick-uniquifier "_")
-  (erc-modules '(notifications hl-nicks image services track))
-  :custom-face
-  (erc-notice-face ((t (:foreground "#ababab"))))
+  (erc-log-channels-directory (expand-file-name ".erc-logs" user-emacs-directory))
+  :bind
+  (("M-z i" . erc-start-or-switch)
+   ("M-m i" . erc-start-or-switch)
+   ("C-c C-b" . erc-switch-to-buffer)
+   (:map erc-mode-map
+         ("M-RET" . newline)))
   :hook
   (ercn-notify . erc-notify)
   :config
-  (make-directory erc-log-channels-directory t)
-  (erc-track-mode 1)
+  (make-directory (expand-file-name ".erc-logs" user-emacs-directory) t)
+  (add-to-list 'erc-modules 'notifications)
+  (erc-track-mode t)
   (erc-services-mode 1)
+  (defun erc-start-or-switch ()
+    "Start ERC or switch to ERC buffer if it has started already."
+    (interactive)
+    (if (get-buffer "irc.libera.chat:6697")
+        (erc-track-switch-buffer 1)
+      (erc-tls :server "irc.libera.chat" :port 6697 :nick my-irc-nick :full-name user-full-name)))
+
   (defun erc-notify (nickname message)
-    "Show notification for ERC message."
+    "Displays a notification message for ERC."
     (let* ((channel (buffer-name))
-           (nick (or (erc-hl-nicks-trim-irc-nick nickname) nickname))
+           (nick (erc-hl-nicks-trim-irc-nick nickname))
            (title (if (string-match-p (concat "^" nickname) channel)
                       nick
-                    (format "%s (%s)" nick channel)))
-           (msg (string-trim (replace-regexp-in-string "[ \t\n]+" " " message))))
+                    (concat nick " (" channel ")")))
+           (msg (s-trim (s-collapse-whitespace message))))
       (alert (concat nick ": " msg) :title title))))
+;; -ERCPac
 
 (provide 'init-erc)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
