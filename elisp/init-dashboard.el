4f4a24dc51c931d954ef6f7822ff8a12549f238a
zorowk <near.kingzero@gmail.com>
2026-03-24 11:19:24 +0800

    update modus vivendi

--- elisp/init-dashboard.el
+++ elisp/init-dashboard.el
@@ -49,7 +49,7 @@
   :custom
   (dashboard-icon-type 'all-the-icons)
   (dashboard-banner-logo-title "Close the world. Open the nExt.")
-  (dashboard-startup-banner (expand-file-name "images/KEC_Light_BK_Small.png" user-emacs-directory))
+  (dashboard-startup-banner (expand-file-name "images/KEC_Dark_BK_Small.png" user-emacs-directory))
   (dashboard-items '((recents  . 7)
                      (bookmarks . 7)
                      (agenda . 5)))
