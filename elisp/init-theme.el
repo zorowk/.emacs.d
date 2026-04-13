67b45d41fab20c85a58f4e6e9b568853a644a889
zorowk <nearkingzero@outlook.com>
2026-04-14 00:09:46 +0800

    update theme to modus-operandi

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -39,7 +39,7 @@
 (eval-when-compile
   (require 'init-const))
 
-(load-theme 'modus-operandi-tinted :no-confirm)
+(load-theme 'modus-operandi :no-confirm)
 
 ;; SpaciousPadding
 (use-package spacious-padding
