63d66542598c7260661feb9dc3ed43109606e64f
zorowk <nearkingzero@outlook.com>
2026-05-11 00:23:34 +0800

    Simplify Scrolling Settings

--- elisp/init-scroll.el
+++ elisp/init-scroll.el
@@ -35,21 +35,12 @@
 ;;
 ;;; Code:
 
-;; SmoothScroll
-;; Vertical Scroll
-(setq scroll-step 1)
+;; Scroll
+;; Keep keyboard scrolling stable; pixel-scroll-precision-mode handles touchpads.
 (setq scroll-margin 1)
 (setq scroll-conservatively 101)
-(setq scroll-up-aggressively 0.01)
-(setq scroll-down-aggressively 0.01)
-(setq auto-window-vscroll nil)
-(setq fast-but-imprecise-scrolling nil)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (setq mouse-wheel-progressive-speed nil)
-;; Horizontal Scroll
-(setq hscroll-step 1)
-(setq hscroll-margin 1)
-;; -SmoothScroll
+;; -Scroll
 
 (provide 'init-scroll)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
