394a7e0e031775b624905955f3b53f87e31f556e
彭文浩 <ut000203@pengwenhao>
2026-05-27 13:21:38 +0800

    update font configure

--- elisp/init-fonts.el
+++ elisp/init-fonts.el
@@ -38,26 +38,40 @@
 ;; FontFun
 (defun change-font ()
   ;; 1. Setup Default Face (Monospaced/Coding)
-  (set-face-attribute 'default nil
-                      :family "JetBrains Mono"
-                      :height 150)
+  (let ((font-height (if (eq system-type 'darwin) 150 110)))
+    (set-face-attribute 'default nil
+                        :family "JetBrains Mono"
+                        :height font-height))
 
   ;; 2. Setup Variable-Pitch Face (Prose/Safari Style)
   ;; We manually set this since we are not using fontaine.
   (set-face-attribute 'fixed-pitch nil :family "JetBrains Mono" :height 1.0)
-  (set-face-attribute 'variable-pitch nil :family "Georgia" :height 1.0)
+  (if (eq system-type 'darwin)
+      (set-face-attribute 'variable-pitch nil :family "Georgia" :height 1.0)
+    (set-face-attribute 'variable-pitch nil :family "Gelasio" :height 1.0))
 
   ;; 3. Symbol and Emoji Configuration
-  (set-fontset-font t 'emoji (font-spec :family "Apple Color Emoji"))
-  (set-fontset-font t 'symbol (font-spec :family "STIX Two Math"))
-  (set-fontset-font t 'greek (font-spec :family "Apple Symbols"))
-
-  ;; 4. CJK Character Configuration (Linux optimized)
-  (set-fontset-font t 'hangul (font-spec :family "Apple SD Gothic Neo"))
-  (set-fontset-font t 'kana (font-spec :family "Hiragino Maru Gothic ProN"))
-  (set-fontset-font t 'cjk-misc (font-spec :family "PingFang SC"))
-  (set-fontset-font t 'bopomofo (font-spec :family "PingFang SC"))
-  (set-fontset-font t 'han (font-spec :family "PingFang SC")))
+  (if (eq system-type 'darwin)
+      (progn
+        ;; Symbols & Emojis
+        (set-fontset-font t 'emoji (font-spec :family "Apple Color Emoji"))
+        (set-fontset-font t 'symbol (font-spec :family "STIX Two Math"))
+        (set-fontset-font t 'greek (font-spec :family "Apple Symbols"))
+        ;; CJK
+        (set-fontset-font t 'hangul (font-spec :family "Apple SD Gothic Neo"))
+        (set-fontset-font t 'kana (font-spec :family "Hiragino Maru Gothic ProN"))
+        (set-fontset-font t 'cjk-misc (font-spec :family "PingFang SC"))
+        (set-fontset-font t 'bopomofo (font-spec :family "PingFang SC"))
+        (set-fontset-font t 'han (font-spec :family "PingFang SC")))
+    (progn
+      ;; Symbols & Emojis
+      (set-fontset-font t 'emoji (font-spec :family "Noto Color Emoji"))
+      (set-fontset-font t 'symbol (font-spec :family "Noto Sans Math"))
+      (set-fontset-font t 'greek (font-spec :family "Noto Sans Symbols"))
+      ;; CJK
+      (set-fontset-font t 'hangul (font-spec :family "Noto Sans CJK KR"))
+      (set-fontset-font t 'kana (font-spec :family "Noto Sans CJK JP"))
+      (set-fontset-font t 'han (font-spec :family "Noto Sans CJK SC")))))
 
 (when (display-graphic-p)
   (change-font))
