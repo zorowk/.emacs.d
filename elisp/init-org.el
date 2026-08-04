7d119a9f21e4e2d00f92ab21b49877e982631faa
zorowk <nearkingzero@outlook.com>
2026-08-04 23:09:35 +0800

    add denote-journal tool

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -230,6 +230,28 @@ by Org before the current buffer is saved."
   ;; Automatically rename Denote buffers using the `denote-rename-buffer-format'.
   (denote-rename-buffer-mode 1))
 
+(use-package denote-journal
+  :ensure t
+  :commands
+  (denote-journal-new-entry
+   denote-journal-new-or-existing-entry
+   denote-journal-link-or-create-entry)
+  :hook
+  (calendar-mode . denote-journal-calendar-mode)
+  :bind
+  (("C-c n j" . denote-journal-new-or-existing-entry))
+  :config
+  ;; Use the "journal" subdirectory of the `denote-directory'.  Set this
+  ;; to nil to use the `denote-directory' instead.
+  (setq denote-journal-directory
+        (expand-file-name "journal" denote-directory))
+  ;; Default keyword for new journal entries. It can also be a list of
+  ;; strings.
+  (setq denote-journal-keyword "journal")
+  ;; Read the doc string of `denote-journal-title-format'.
+  (setq denote-journal-title-format
+        'day-date-month-year))
+
 (use-package consult-denote
   :ensure t
   :bind
